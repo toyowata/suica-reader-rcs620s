ec4456f0ee044c3a1f2a72d346226823cbd8d734
Toyomasa Watarai <toyomasa.watarai@gmail.com>
2021-07-11 11:56:23 +0900

    Add station data, printer support

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "USBSerial.h"
 #include "SB1602E.h"
 #include "RCS620S.h"
+#include "AS289R2.h"
+#include "sc_utf8.h"
 
 // RCS620S
 #define PUSH_TIMEOUT                  2100
@@ -27,215 +29,25 @@
 int requestService(uint16_t serviceCode);
 int readEncryption(uint16_t serviceCode, uint8_t blockNumber, uint8_t *buf);
 void printBalanceLCD(const char *card_name, uint32_t balance);
+void parse_history(uint8_t *buf);
+void get_station(char *buf, int line, int station);
+
+const int record_length = (3 + 40 + 40);
 
 DigitalOut led(LED1);
 USBSerial serial(true);
-SB1602E lcd(p14, p15);
-RCS620S rcs620s(p16, p17);
-
-void parse_history(uint8_t *buf)
-{
-    char info[80], info2[40];
-    int region_in, region_out, line_in, line_out, station_in, station_out;
-
-    region_in = (buf[0xf] >> 6) & 3;
-    region_out = (buf[0xf] >> 4) & 3;
-    line_in = buf[6];
-    line_out = buf[7];
-    station_in = buf[8];
-    station_out = buf[9];
-
-    for(int i = 0; i < 16; i++) {
-        serial.printf("%02X ", buf[i]);
-    }
-    serial.printf("\n");
-
-    sprintf(info, "機種種別: ");
-    switch (buf[0]) {
-        case 0x03:
-            strcat(info, "のりこし精算機\r");
-            break;
-        case 0x05:
-            strcat(info, "バス/路面等\r");
-            break;
-        case 0x07:
-        case 0x08:
-        case 0x12:
-            strcat(info, "自動券売機\r");
-            break;
-        case 0x14:
-            strcat(info, "駅窓口\r");
-            break;
-        case 0x15:
-            strcat(info, "定期券発売機\r");
-            break;            
-        case 0x16:
-            strcat(info, "自動改札機\r");
-            break;
-        case 0x17:
-            strcat(info, "簡易改札機\r");
-            break;
-        case 0x18:
-            strcat(info, "駅務機器\r");
-            break;
-        case 0x46:
-            strcat(info, "ビューアルッテ端末\r");
-            break;
-        case 0xc7:
-        case 0xc8:
-            strcat(info, "物販端末\r");
-            break;
-        default:
-            strcat(info, "不明\r");
-            break;
-    }
-    serial.printf("%s", info);
-    // tp.printf("%s", info);
-
-    sprintf(info, "利用種別: ");
-    switch (buf[1]) {
-        case 0x01:
-            strcat(info, "自動改札出場\r");
-            break;
-        case 0x02:
-            strcat(info, "SFチャージ\r");
-            break;
-        case 0x03:
-            strcat(info, "きっぷ購入\r");
-            break;
-        case 0x04:
-            strcat(info, "磁気券精算\r");
-            break;
-        case 0x05:
-            strcat(info, "乗越精算\r");
-            break;
-        case 0x06:
-            strcat(info, "窓口精算\r");
-            break;
-        case 0x07:
-            strcat(info, "新規\r");
-            break;
-        case 0x08:
-            strcat(info, "チャージ控除\r");
-            break;
-        case 0x0C:
-        case 0x0D:
-        case 0x0F:
-            strcat(info, "バス/路面等\r");
-            break;
-        case 0x14:
-            strcat(info, "オートチャージ\r");
-            break;
-        case 0x46:
-            strcat(info, "物販\r");
-            break;
-        default:
-            strcat(info, "不明\r");
-            break;
-    }
-    serial.printf("%s", info);
-    // tp.printf("%s", info);
-
-    if (buf[2] != 0) {
-        sprintf(info, "支払種別: ");
-        switch (buf[2]) {
-            case 0x02:
-                strcat(info, "VIEW\r");
-                break;
-            case 0x0B:
-                strcat(info, "PiTaPa\r");
-                break;
-            case 0x0d:
-                strcat(info, "オートチャージ対応PASMO\r");
-                break;
-            case 0x3f:
-                strcat(info, "モバイルSuica\r");
-                break;
-            default:
-                strcat(info, "不明\r");
-                break;
-        }
-        serial.printf("%s", info);
-        // tp.printf("%s", info);
-    }
-
-    if (buf[1] == 0x01 || buf[1] == 0x14) {
-        sprintf(info, "入出場種別: ");
-        switch (buf[3]) {
-            case 0x01:
-                strcat(info, "入場");
-                sprintf(info2, " %02X %02X", buf[6], buf[7]);
-                strcat(info, info2);
-                break;
-            case 0x02:
-                strcat(info, "出場");
-                sprintf(info2, " %02X %02X %02X %02X", buf[6], buf[7], buf[8], buf[9]);
-                strcat(info, info2);
-                break;
-            case 0x03:
-                strcat(info, "定期入場");
-                sprintf(info2, " %02X %02X", buf[6], buf[7]);
-                strcat(info, info2);
-                break;
-            case 0x04:
-                strcat(info, "定期出場");
-                sprintf(info2, " %02X %02X %02X %02X", buf[6], buf[7], buf[8], buf[9]);
-                strcat(info, info2);
-                break;
-            case 0x0E:
-                strcat(info, "窓口出場");
-                break;
-            case 0x0F:
-                strcat(info, "バス入出場");
-                break;
-            case 0x12:
-                strcat(info, "料金定期");
-                break;
-            case 0x17:
-            case 0x1D:
-                strcat(info, "乗継割引");
-                break;
-            case 0x21:
-                strcat(info, "バス等乗継割引");
-                break;
-            case 0x22:
-            case 0x25:
-            case 0x26:
-                strcat(info, "券面外乗降");
-                sprintf(info2, " [%d] %02X %02X [%d] %02X %02X", (buf[0xf] >> 6) & 3, buf[6], buf[7], (buf[0xf] >> 4) & 3, buf[8], buf[9]);
-                strcat(info, info2);
-                break;
-            default:
-                strcat(info, "不明");
-                sprintf(info2, " %02X %02X %02X %02X", buf[6], buf[7], buf[8], buf[9]);
-                strcat(info, info2);
-                break;
-        }
-        serial.printf("%s\r", info);
-        // tp.printf("%s\r", info);
-    }
-
-    sprintf(info, "処理日付: %d/%02d/%02d", 2000+(buf[4]>>1), ((buf[4]&1)<<3 | ((buf[5]&0xe0)>>5)), buf[5]&0x1f);
-    if (buf[1] == 0x46) {   // 物販
-        sprintf(info2, " %02d:%02d:%02d", (buf[6] & 0xF8) >> 3, ((buf[6] & 0x7) >> 5) | ((buf[7] & 0xe0) >> 5), (buf[7] & 0x1f));
-        strcat(info, info2);
-    }
-    strcat(info, "\r");
-    serial.printf("%s", info);
-    // tp.printf("%s", info);
-
-    sprintf(info, "残額: %d円\r\r", (buf[11]<<8) + buf[10]); 
-    serial.printf("%s\n", info);
-    // tp.setDoubleSizeWidth();
-    // tp.printf("%s", info);
-    // tp.clearDoubleSizeWidth();
-}
+SB1602E lcd(I2C_LCD_SDA, I2C_LCD_SCL);
+RCS620S rcs620s(RCS620S_TX, RCS620S_RX);
+AS289R2 tp(AS289R2_TX, AS289R2_RX);
 
 int main()
 {
     uint8_t buffer[20][16];
     uint8_t idm[8];
     
+    //serial.init();
+    //serial.connect();
+
     lcd.setCharsInLine(8);
     lcd.clear();
     lcd.contrast(0x35);
@@ -246,8 +58,8 @@ int main()
     serial.printf("\n*** RCS620S テストプログラム ***\n\n");
 
     rcs620s.initDevice();
-    // tp.initialize();
-    // tp.putLineFeed(1);
+    tp.initialize();
+    tp.putLineFeed(1);
     memset(idm, 0, 8);
 
     while(1) {
@@ -323,15 +135,14 @@ int main()
             // 残高表示 (Suica or PASMO)
             balance = buffer[0][11];                  // 11 byte目
             balance = (balance << 8) + buffer[0][10]; // 10 byte目
-            lcd.clear();
-            lcd.printf(0, 0, (char*)"Suica");
-            lcd.printf(0, 1, (char*)"\\ %d", balance);
-            for (int i = 0; i < PRINT_ENTRIES; i++) {
+            // 残高表示
+            printBalanceLCD("Suica", balance);
+            for (int i = (PRINT_ENTRIES - 1); i >= 0; i--) {
                 if (buffer[i][0] != 0) {
                     parse_history(&buffer[i][0]);
                 }
             }
-            // tp.putLineFeed(4);
+            tp.putLineFeed(4);
             isCaptured = 0;
         }
 
@@ -341,8 +152,258 @@ int main()
     }
 }
 
+void parse_history(uint8_t *buf)
+{
+    char info[80+40], info2[40+40];
+    int region_in, region_out, line_in, line_out, station_in, station_out;
 
-// request service
+    region_in = (buf[0xf] >> 6) & 3;
+    region_out = (buf[0xf] >> 4) & 3;
+    line_in = buf[6];
+    line_out = buf[8];
+    station_in = buf[7];
+    station_out = buf[9];
+
+    for(int i = 0; i < 16; i++) {
+        serial.printf("%02X ", buf[i]);
+    }
+    serial.printf("\n");
+
+    sprintf(info, "機種種別: ");
+    switch (buf[0]) {
+        case 0x03:
+            strcat(info, "のりこし精算機\r");
+            break;
+        case 0x04:
+            strcat(info, "携帯型端末\r");
+            break;
+        case 0x05:
+            strcat(info, "バス/路面等\r");
+            break;
+        case 0x09:
+            strcat(info, "入金機\r");
+            break;
+        case 0x07:
+        case 0x08:
+        case 0x12:
+            strcat(info, "券売機\r");
+            break;
+        case 0x14:
+        case 0x15:
+            strcat(info, "券売機等\r");
+            break;
+        case 0x16:
+            strcat(info, "自動改札機\r");
+            break;
+        case 0x17:
+            strcat(info, "簡易改札機\r");
+            break;
+        case 0x18:
+        case 0x19:
+            strcat(info, "窓口端末\r");
+            break;
+        case 0x1A:
+            strcat(info, "改札端末\r");
+            break;
+        case 0x1B:
+            strcat(info, "モバイルFeliCa\r");
+            break;
+        case 0x1C:
+            strcat(info, "乗継精算機\r");
+            break;
+        case 0x1D:
+            strcat(info, "連絡改札機\r");
+            break;
+        case 0x1F:
+            strcat(info, "簡易入金機\r");
+            break;
+        case 0x23:
+            strcat(info, "乗継精算機\r");
+            break;
+        case 0x46:
+        case 0x48:
+            strcat(info, "ビューアルッテ端末\r");
+            break;
+        case 0xc7:
+            strcat(info, "物販端末\r");
+            break;
+        case 0xc8:
+            strcat(info, "自販機\r");
+            break;
+        default:
+            strcat(info, "不明\r");
+            break;
+    }
+    serial.printf("%s", info);
+    tp.printf("%s", info);
+
+    int hasStationName = 0;
+    sprintf(info, "利用種別: ");
+    switch (buf[1]) {
+        case 0x01:
+            strcat(info, "自動改札出場");
+            break;
+        case 0x02:
+            strcat(info, "SFチャージ\r");
+            hasStationName = 1;
+            break;
+        case 0x03:
+            strcat(info, "きっぷ購入\r");
+            hasStationName = 1;
+            break;
+        case 0x04:
+            strcat(info, "磁気券精算");
+            break;
+        case 0x05:
+            strcat(info, "乗越精算");
+            break;
+        case 0x06:
+            strcat(info, "窓口精算");
+            break;
+        case 0x07:
+            strcat(info, "新規\r");
+            hasStationName = 1;
+            break;
+        case 0x08:
+            strcat(info, "チャージ控除");
+            break;
+        case 0x0C:
+        case 0x0D:
+        case 0x0F:
+            strcat(info, "バス/路面等");
+            break;
+        case 0x13:
+            strcat(info, "支払い（新幹線利用）\r");
+            hasStationName = 2;
+            break;
+        case 0x14:
+            strcat(info, "オートチャージ");
+            break;
+        case 0x46:
+            strcat(info, "物販");
+            break;
+        case 0xc6:
+            strcat(info, "現金併用物販");
+            break;
+        default:
+            strcat(info, "不明");
+            break;
+    }
+    if (hasStationName >= 1) {
+        get_station(info2, line_in, station_in);
+        strcat(info, info2);
+    }
+    if (hasStationName == 2) {
+        strcat(info, " - ");
+        get_station(info2, line_out, station_out);
+        strcat(info, info2);
+    }
+    serial.printf("%s\r", info);
+    tp.printf("%s\r", info);
+
+#if 0
+    if (buf[2] != 0) {
+        sprintf(info, "支払種別: ");
+        switch (buf[2]) {
+            case 0x02:
+                strcat(info, "VIEW\r");
+                break;
+            case 0x0B:
+                strcat(info, "PiTaPa\r");
+                break;
+            case 0x0d:
+                strcat(info, "オートチャージ対応PASMO\r");
+                break;
+            case 0x3f:
+                strcat(info, "モバイルSuica\r");
+                break;
+            default:
+                strcat(info, "不明\r");
+                break;
+        }
+        serial.printf("%s", info);
+        tp.printf("%s", info);
+    }
+#endif
+
+    hasStationName = 0;
+    if (buf[1] == 0x01 || buf[1] == 0x14) {
+        sprintf(info, "入出場種別: ");
+        switch (buf[3]) {
+            case 0x01:
+            case 0x08:
+                strcat(info, "入場\r");
+                hasStationName = 1;
+                break;
+            case 0x02:
+                strcat(info, "出場\r");
+                hasStationName = 2;
+                break;
+            case 0x03:
+                strcat(info, "定期入場\r");
+                hasStationName = 1;
+                break;
+            case 0x04:
+                strcat(info, "定期出場\r");
+                hasStationName = 2;
+                break;
+            case 0x0E:
+                strcat(info, "窓口出場");
+                break;
+            case 0x0F:
+                strcat(info, "バス入出場");
+                break;
+            case 0x12:
+                strcat(info, "料金定期");
+                break;
+            case 0x17:
+            case 0x1D:
+                strcat(info, "乗継割引");
+                break;
+            case 0x21:
+                strcat(info, "バス等乗継割引");
+                break;
+            case 0x22:
+            case 0x25:
+            case 0x26:
+                strcat(info, "券面外乗降\r");
+                hasStationName = 2;
+                break;
+            default:
+                strcat(info, "不明");
+                sprintf(info2, " %02X %02X %02X %02X", buf[6], buf[7], buf[8], buf[9]);
+                strcat(info, info2);
+                break;
+        }
+        if (hasStationName >= 1) {
+            get_station(info2, line_in, station_in);
+            strcat(info, info2);
+        }
+        if (hasStationName == 2) {
+            strcat(info, " - ");
+            get_station(info2, line_out, station_out);
+            strcat(info, info2);
+        }
+        serial.printf("%s\r", info);
+        tp.printf("%s\r", info);
+    }
+
+    sprintf(info, "処理日付: %d/%02d/%02d", 2000+(buf[4]>>1), ((buf[4]&1)<<3 | ((buf[5]&0xe0)>>5)), buf[5]&0x1f);
+    if (buf[1] == 0x46) {   // 物販
+        sprintf(info2, " %02d:%02d:%02d", (buf[6] & 0xF8) >> 3, ((buf[6] & 0x7) >> 5) | ((buf[7] & 0xe0) >> 5), (buf[7] & 0x1f));
+        strcat(info, info2);
+    }
+    strcat(info, "\r");
+    serial.printf("%s", info);
+    tp.printf("%s", info);
+
+    sprintf(info, "残額: %d円\r\r", (buf[11]<<8) + buf[10]); 
+    serial.printf("%s\n", info);
+    tp.setDoubleSizeWidth();
+    tp.printf("%s", info);
+    tp.clearDoubleSizeWidth();
+}
+
 int requestService(uint16_t serviceCode){
     int ret;
     uint8_t buf[RCS620S_MAX_CARD_RESPONSE_LEN];
@@ -389,8 +450,29 @@ int readEncryption(uint16_t serviceCode, uint8_t blockNumber, uint8_t *buf){
 
 void printBalanceLCD(const char *card_name, uint32_t balance)
 {
-    printf("%s: %ld\n", card_name, balance);
     lcd.clear();
     lcd.printf(0, 0, (char*)"%s", card_name);
     lcd.printf(0, 1, (char*)"\\ %d", balance);
+}
+
+void get_station(char *buf, int line, int station) {
+    unsigned int offset = 0;
+    while(1) {
+        if (sc_utf8[1 + offset] == line) {
+            if (sc_utf8[2 + offset] == station) {
+                sprintf(buf, "%s線 %s駅", &sc_utf8[3 + offset], &sc_utf8[3 + 40 + offset]);
+                break;
+            }
+            else {
+                offset += record_length;
+            }
+        }
+        else {
+            offset += record_length;
+        }
+        if (offset > sc_utf8_len) {
+            sprintf(buf, "---");
+            break;
+        }
+    }
 }
